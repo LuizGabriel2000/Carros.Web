039ee4f33dd9b8e842877183d3f3fd042ee63d99
LuizGabriel2000 <luizgabriel.lg2000@outlook.com>
2022-07-04 20:23:31 -0300

    Adicionado a Requisição de edição aos carros, e implementada novas colunas (marca e cor)

--- src/components/commons/NavBar.vue
+++ src/components/commons/NavBar.vue
@@ -7,24 +7,31 @@
         >
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Controle De Carros </v-toolbar-title>
+      <v-toolbar-title>Controle De Carros</v-toolbar-title>
 
       <v-spacer></v-spacer>
-
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
+      <div class="d-flex" style="width: 400px">
+        <v-text-field @input="buscar(pesquisa)" v-model="pesquisa" label="Buscar veiculo"></v-text-field>
+        <v-btn @click="buscar(pesquisa)" icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+      </div>
     </v-app-bar>
     </div>
 </template>
 
 <script>
 export default {
-
+  props: {
+    buscar: Function
+  },
+  data() {
+    return {
+      pesquisa: ''
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
--- src/views/HomeCarros.vue
+++ src/views/HomeCarros.vue
@@ -1,7 +1,7 @@
 <template>
 <div style="height: 100%">
     <div class="img-fundo">
-        <NavBarVue/>
+        <NavBarVue :buscar="(pesquisa) => buscar(pesquisa)"/>
         <v-container>
             <v-card  class="my-10" color="rgba(0, 0, 0, 0.220)">
                 <v-row>
@@ -10,12 +10,12 @@
                     </v-col>
 
                     <v-col cols="3">
-                        <v-text-field v-model="formData.modelo" :rules="rules"></v-text-field>
+                        <v-text-field v-model="formData.modelo"></v-text-field>
                             <p style="color: maroon;">Marca - Modelo (Exemplo: "Fiat Uno")</p>
                     </v-col>
 
                     <v-col cols="3">
-                         <v-text-field v-model="formData.placa" :rules="rules"></v-text-field>
+                         <v-text-field v-model="formData.placa"></v-text-field>
                           <p style="color: maroon;" >Placa (Exemplo: "HSP2367")</p>
                     </v-col>
 
@@ -30,18 +30,29 @@
 
             <v-card class="my-15" color="rgba(0, 0, 0, 0.290)">
                 <v-row>
-                    <v-col cols="4">
+                    <v-col cols="3">
                         <h2 style="color: beige;" class="ma-4">Lista dos Carros:</h2>
                     </v-col>
 
-                    <v-col cols="8">
+                    <v-col cols="9">
                         <v-row>
-                            <v-col cols="5">
-                                <h3 style="color: blue;">Marca e Modelo</h3>
+                            <v-col cols="3">
+                                <h3 style="color: blue;">Marca</h3>
                             </v-col>
 
-                            <v-col cols="7">
+                            <v-col cols="3">
+                                <h3 style="color: blue;">Modelo</h3>
+                            </v-col>
+
+                            <v-col cols="2">
+                                <h3 style="color: blue;">Cor</h3>
+                            </v-col>
+
+                            <v-col cols="2">
                                 <h3 style="color: blue;">Placa</h3>
+                            </v-col>
+
+                            <v-col cols="2">
                             </v-col>
                         </v-row>
                         
@@ -54,28 +65,60 @@
                             <template v-slot:default="{ item }">
                                 <v-list-item :key="item">
                                     <v-row>
-                                        <v-col cols="5">
-                                            <h3>{{item.descricao}}</h3>
+                                        <v-col cols="3">
+                                            <h3>{{item.marca}}</h3>
                                         </v-col>
 
                                         <v-col cols="3">
-                                            
+                                            <h3>{{item.descricao}}</h3>
+                                        </v-col>
+
+                                        <v-col cols="2">
+                                            <h3>{{item.cor}}</h3>
+                                        </v-col>
+
+
+
+                                        <v-col cols="2">                                       
                                             <div id="placa-carro" class="placa-carro">
                                                 <div class="targeta">BRASIL</div>
                                                 {{item.placa}}
                                             </div>
                                         </v-col>
 
-                                        <v-col cols="4">
+                                        <v-col cols="1">
+                                            <v-btn
+                                                color="#fff"
+                                                depressed
+                                                icon
+                                                @click="EditarCarro(item)"
+                                            >
+                                            <v-icon color="primary">mdi-wrench</v-icon> 
+
+                                            </v-btn>
+
+                                            
+
+                                        </v-col>
+
+
+
+                                         <v-col cols="1">
                                             <v-btn
                                                 color="#fff"
                                                 depressed
                                                 icon
                                                 @click="DeletarCarro(item.codigo)"
                                             >
-                                            <v-icon color="red">mdi-close</v-icon>
+                                            <v-icon color="red">mdi-close</v-icon> 
+
                                             </v-btn>
+
+                                            
+
                                         </v-col>
+
+                                        
                                     </v-row>
                                 </v-list-item>
                                 <v-divider></v-divider>
@@ -89,7 +132,48 @@
         </v-container>
       
     </div>
-    
+    <v-dialog
+      v-model="dialog"
+      persistent
+      max-width="480"
+    >
+      <v-card>
+        <v-card-title class="text-h5">
+          Editar Carro
+        </v-card-title>
+
+        <v-card-text class="d-flex">
+            <div class="mx-3">
+                <v-text-field v-model="formEdit.marca" label="Marca"></v-text-field>
+                <v-text-field v-model="formEdit.modelo" label="Modelo"></v-text-field>
+            </div>
+            <div class="mx-3">
+                <v-text-field v-model="formEdit.cor" label="Cor"></v-text-field>
+                <v-text-field v-model="formEdit.placa" label="Placa"></v-text-field>
+            </div>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-btn
+            color="primary"
+            text
+            @click="dialog = false"
+          >
+            Fechar
+          </v-btn>
+
+          <v-spacer></v-spacer>
+          
+          <v-btn
+            color="primary"
+            text
+            @click="Salvar()"
+          >
+            Salvar
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
 </div>
  
 </template>
@@ -103,14 +187,23 @@ export default {
   },
   data() {
     return {       
+        dialog: false,
         benched: 0, 
+        lista: [],
         listaCarro: [],
         formData: {
             modelo: null,
             placa:null
         },
+        formEdit: {
+            codigo: null,
+            marca: null,
+            modelo: null,
+            placa: null,
+            cor: null
+        },
         modeloCarro: '',
-        placaCarro: ''
+        placaCarro: '',
     }
   },
 
@@ -133,14 +226,60 @@ export default {
     BuscarCarros() {
         axios
         .get(`http://localhost:3000/api/carros`)
-        .then(response => (this.listaCarro = response.data.result))
+        .then(response => {
+            this.lista = response.data.result
+            this.listaCarro = response.data.result
+        })
     },
 
     DeletarCarro(codigo) {
-        axios.delete(`http://localhost:3000/api/carro/${codigo}`).then((res) => {
-            console.log("apagou com secesso", res.data)
+        axios.delete(`http://localhost:3000/api/carro/${codigo}`)
+        .then((res) => {
+            console.log("Removido com sucesso", res.data)
             this.BuscarCarros()
         })
+    },
+
+    EditarCarro(item) {
+        console.log(item)
+        this.formEdit.marca = item.marca
+        this.formEdit.cor = item.cor
+        this.formEdit.codigo = item.codigo
+        this.formEdit.modelo = item.descricao
+        this.formEdit.placa = item.placa
+        this.dialog = true
+
+    },
+
+    Salvar() {
+        axios.put(`http://localhost:3000/api/carro/${this.formEdit.codigo}`, this.formEdit)
+        .then((res) => {
+            console.log("Editado com secesso", res.data)
+            this.BuscarCarros()
+            this.dialog = false
+        })
+    },
+
+    buscar(pesquisa) {
+
+        let novaLista = this.lista.reduce((lista, item) => {
+            let modelo = item.descricao.match(new RegExp(pesquisa, "gi"))
+            let placa = item.placa.match(new RegExp(pesquisa, "gi"))
+
+            if (modelo || placa) {
+                lista.push(item)
+            }
+
+            return lista
+        }, [])
+
+
+        if (pesquisa) {
+            this.listaCarro = novaLista
+        } else {
+            this.listaCarro = this.lista
+        }
+
     }
   }
 }
